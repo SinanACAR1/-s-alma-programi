fc0f89c51652fd1eadb8cf1ffdc03d45f4da60a7
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 23:30:39 +0300

    Add files via upload

---
+++ üs alma programı/üs alma programı/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace üs_alma_programı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double üs,üstüalınacaksayı;
+            double sonuc;
+
+            üs = Convert.ToDouble(textBox2.Text);
+            üstüalınacaksayı = Convert.ToDouble(textBox1.Text);
+
+            sonuc = Math.Pow(üstüalınacaksayı, üs);
+            label2.Text = sonuc.ToString();
+        }
+    }
+}
